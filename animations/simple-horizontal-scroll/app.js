e67f7c0e697073c1ec451ebfe44eb56d4ac447c1
Tom Okafor <tmeka008@gmail.com>
2026-01-15 16:40:10 +0100

    feat: add lenis for smooth scroll and animate text in the sections above and below the scroller

--- animations/simple-horizontal-scroll/app.js
+++ animations/simple-horizontal-scroll/app.js
@@ -2,8 +2,19 @@ gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const scroller = document.querySelector(".scroller");
 const sections = gsap.utils.toArray(".scroll-item");
-console.log(scroller.clientWidth);
-console.log(sections.length);
+
+const mainHeading = document.querySelector("h1");
+const split = new SplitText(mainHeading, {
+  type: "chars, words",
+  mask: "chars",
+});
+const paragraphSplit = new SplitText(".last-section p", {
+  type: "chars, words",
+  mask: "chars",
+});
+gsap.set(split.chars, { xPercent: -110 });
+gsap.set(mainHeading, { autoAlpha: 1 });
+gsap.to(split.chars, { xPercent: 0, stagger: 0.01, ease: "power4.in" });
 let scrollerTween = gsap.to(scroller, {
   xPercent: 100 / sections.length - 100,
   ease: "none",
@@ -21,17 +32,29 @@ sections.forEach((section) => {
   gsap.from(split.chars, {
     opacity: 0,
     filter: "blur(4px)",
-    stagger: 0.02,
+    stagger: 0.008,
     y: 50,
     ease: "power3.out",
     scrollTrigger: {
       trigger: section,
       start: "left 35%",
-      end: 'right 55%',
+      end: "right 55%",
       containerAnimation: scrollerTween,
       toggleActions: "play reverse play reverse",
     },
   });
+});
+
+gsap.from(paragraphSplit.chars, {
+  xPercent: 110,
+  stagger: {each: 0.01, from: 'end'},
+  ease: 'power4.in',
+  scrollTrigger: {
+    trigger: ".last-section",
+    start: "top 40%",
+    end: "bottom center",
+    toggleActions: 'play none none reverse'
+  },
 });
 
 const lenis = new Lenis({ autoRaf: true });
